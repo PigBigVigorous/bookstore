b800a61910a51fcbd5612a3541a6ec3efa846b30
PigBigVigorous <giakhang12704@gmail.com>
2025-11-29 13:51:12 +0700

    s

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    //
+    use HasFactory;
+
+    // Khai báo các cột được phép gán giá trị hàng loạt
+    protected $fillable = [
+        'user_id', 
+        'product_id', 
+        'quantity'
+    ];
+
+    // Thiết lập mối quan hệ: Một mục giỏ hàng thuộc về một sản phẩm
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    
+    // Thiết lập mối quan hệ: Một mục giỏ hàng thuộc về một người dùng
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
